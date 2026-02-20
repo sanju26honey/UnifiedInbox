10f7b63eb2cb789b6499debe89e6e42c68a833aa
Sanju S <sanju26honey@gmail.com>
2026-02-20 20:32:51 +0530

    feat: landing page with cta

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      
+    <div className="min-h-screen flex justify-center items-center">
+      <div className="text-center">
+        <h1 className="text-5xl">Unified Inbox</h1><br /><br />
+        <Link href="/dashboard" className="p-3 px-8 bg-red-500 rounded-xl">Get Started</Link>
+      </div>
     </div>
   );
 }
