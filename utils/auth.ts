eb71b45b090f87749f85b55f1e7c4d3e0cdc85d7
Sanju S <sanju26honey@gmail.com>
2026-02-22 03:39:05 +0530

    Feat: Store authenticated users in DB

--- utils/auth.ts
+++ utils/auth.ts
@@ -1,8 +1,11 @@
 import { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google"
 import GithubProvider from "next-auth/providers/github"
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import { prisma } from "@/lib/prisma"
 
 export const authOptions: NextAuthOptions = {
+    adapter: PrismaAdapter(prisma),
     providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID!,
