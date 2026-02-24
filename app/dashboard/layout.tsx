cf7e501d17c94b6f46d36ea4c354b05f45c690a5
Sanju S <sanju26honey@gmail.com>
2026-02-25 03:54:29 +0530

    refactor: update dashboard to include signout options

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -8,5 +8,23 @@ export default async function DashboardLayout({
 
     await CheckLogin();
 
-    return <>{children}</>
+    return <>
+    <div className="min-h-screen w-full overflow-auto no-scrollbar relative bg-linear-to-tl from-green-500/30 to-blue-500/30">
+    {/* Arctic Lights Background with Top Glow */}
+        <div
+        className="fixed overflow-auto no-scrollbar  inset-0 z-0"
+        style={{
+            background: "radial-gradient(ellipse 80% 60% at 50% 0%, rgba(0, 0, 0, 0.5), transparent 70%), transparent",
+        }}
+        >
+            <div className="fixed overflow-auto no-scrollbar  inset-0 z-0"
+            style={{
+                backgroundColor: "transparent",
+                backgroundImage: "radial-gradient(at 4% 69%, #070f2b 0%, transparent 60%), radial-gradient(at 17% 10%, #1b1a55 0%, transparent 50%), radial-gradient(at 74% 76%, rgba(5, 223, 114, 0.1) 0%, transparent 40%), radial-gradient(at 71% 94%, rgba(5, 223, 114, 0.2) 0%, transparent 30%)"
+            }}>
+                {children}
+            </div>
+        </div>
+    </div>
+    </>
 }
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -5,23 +5,25 @@ import { ChatIcon } from "@phosphor-icons/react/ssr";
 import { WhatsappLogoIcon } from "@phosphor-icons/react/ssr";
 import Link from "next/link";
 import RecentChats from "@/components/RecentChats";
+import SignOut from "@/components/SignOut";
 
 export default async function Dashboard() {
     const session = await getServerSession(authOptions);
   
     return (
         <>
+            <SignOut/>
             <div className="min-h-screen grid grid-cols-4">
-                <div className="border-r-2 px-5 pt-10 border-white/10 overflow-auto max-h-screen no-scrollbar">
+                <div className="md:order-1 md:col-span-1 col-span-4 order-2 border-r-2 px-5 pt-10 border-white/10 overflow-auto max-h-screen no-scrollbar">
                     <RecentChats userPlatform=""/>
                 </div>
-                <div className="flex justify-center items-center p-5 col-span-3">
+                <div className="md:order-2 order-1 h-[60vh] md:h-auto flex justify-center items-center p-5 col-span-4 md:col-span-3">
                     <div className="text-center">
-                        <h1 className="text-5xl">Welcome, {session?.user?.name?.split(" ")[0]}!</h1><br />
-                        <p className="text-sm">Ready to send? Create a message to begin.</p><br />
-                        <div className="flex gap-5 mx-auto w-max">
-                            <Link href="/dashboard/whatsapp" className="bg-green-400 text-black p-3 cursor-pointer inset-shadow-white/10 inset-shadow-2xs shadow-white/10 shadow-2xs m-auto gap-2 rounded-md flex"><WhatsappLogoIcon size={24}/> Whatsapp</Link>
-                            <Link href="/dashboard/sms" className="bg-white text-black p-3 cursor-pointer inset-shadow-white/10 inset-shadow-2xs shadow-white/10 shadow-2xs m-auto gap-2 rounded-md flex"><ChatIcon size={24}/> SMS</Link>
+                        <h1 className="text-4xl md:text-5xl">Welcome, {session?.user?.name?.split(" ")[0]}!</h1><br />
+                        <p className="md:text-sm text-xs">Ready to send? Create a message to begin.</p><br />
+                        <div className="flex gap-5 md:p-0 p-3 mx-auto w-max">
+                            <Link href="/dashboard/whatsapp" className="bg-green-400 text-black md:p-3 px-3 py-1.5 cursor-pointer inset-shadow-white/10 inset-shadow-2xs shadow-white/10 shadow-2xs m-auto gap-2 rounded-md flex"><WhatsappLogoIcon size={24}/> Whatsapp</Link>
+                            <Link href="/dashboard/sms" className="bg-white text-black md:p-3 px-3 py-1.5 cursor-pointer inset-shadow-white/10 inset-shadow-2xs shadow-white/10 shadow-2xs m-auto gap-2 rounded-md flex"><ChatIcon size={24}/> SMS</Link>
                         </div>
                     </div>
                 </div>
